4dc91b84b273d79245768bb0d46d2984f11d9546
Nicholas Car <nicholas.car@csiro.au>
2019-05-08 12:53:32 +1000

    retrying load script - repo create/delete ok

--- scripts/vocabs_load.py
+++ scripts/vocabs_load.py
@@ -2,15 +2,14 @@ import requests
 import json
 import os
 import rdflib
-
-import _config as config
+from scripts import config
 
 
 # create-repos or load-data
-def load_one_vocab_from_github(vocab_id, base_url, label):
+def load_one_vocab_from_github(repo_id, vocab_id, base_url, label):
     namespace = base_url + '/'
     r = requests.post(
-        config.GRAPHDB_LOAD_DATA_URI,
+        config.GRAPHDB_LOAD_DATA_URI.format(repo_id),
         auth=(config.GRAPHDB_USR, config.GRAPHDB_PWD),
         headers={'Content-Type': 'application/json', 'Accept': 'application/json'},
         json={
@@ -48,39 +47,48 @@ def load_one_vocab_from_github(vocab_id, base_url, label):
     return r.status_code
 
 
-def load_all_background_onts_from_github():
+def load_all_background_onts_from_github(repo_id):
     background_onts = [
         {
-            'label': 'RDF Ontology',
-            'namespace': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+            'name': 'RDF Ontology',
+            'namespace': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+            'file': 'rdf.ttl'
         },
         {
-            'label': 'RDFS Ontology',
-            'namespace': 'http://www.w3.org/2000/01/rdf-schema#'
+            'name': 'RDFS Ontology',
+            'namespace': 'http://www.w3.org/2000/01/rdf-schema#',
+            'file': 'rdfs.ttl'
         },
         {
-            'label': 'OWL Ontology',
-            'namespace': 'http://www.w3.org/2002/07/owl#'
+            'name': 'OWL Ontology',
+            'namespace': 'http://www.w3.org/2002/07/owl#',
+            'file': 'owl.ttl'
         },
         {
-            'label': 'SKOS Ontology',
-            'namespace': 'http://www.w3.org/2004/02/skos/core#'
+            'name': 'SKOS Ontology',
+            'namespace': 'http://www.w3.org/2004/02/skos/core#',
+            'file': 'skos.ttl'
         },
     ]
 
     for ont in background_onts:
+        print('Loading {}'.format(ont['name']))
+        print('repo: {}'.format(repo_id))
         r = requests.post(
-            config.GRAPHDB_LOAD_DATA_URI,
+            config.GRAPHDB_LOAD_DATA_URI.format(repo_id),
             auth=(config.GRAPHDB_USR, config.GRAPHDB_PWD),
-            headers={'Content-Type': 'application/json', 'Accept': 'application/json'},
+            headers={
+                'Content-Type': 'application/json',
+                'Accept': 'text/plain'
+            },
             json={
                 'baseURI': ont['namespace'],
                 'context': ont['namespace'],
-                'data': config.GITHUB_RAW_URI_BASE + 'extra-rdf.ttl',
+                'data': config.GITHUB_RAW_URI_BASE + 'ontologies/' + ont['file'],
                 'forceSerial': True,
                 'format': 'text/turtle',
                 'message': '',
-                'name': ont['label'],
+                'name': ont['name'],
                 'parserSettings': {
                     'failOnUnknownDataTypes': True,
                     'failOnUnknownLanguageTags': True,
@@ -107,12 +115,12 @@ def load_all_background_onts_from_github():
             print(r.content.decode('utf-8'))
             return r.status_code
         else:
-            print('Loaded ' + ont['label'])
+            print('Loaded ' + ont['name'])
 
     return True
 
 
-def load_all_vocabs_from_github():
+def load_all_vocabs_from_github(repo_id):
     for file in os.listdir('.'):
         if file.startswith('gsq-') and file.endswith('.ttl'):
             g = rdflib.Graph().parse(file, format='turtle')
@@ -124,15 +132,16 @@ def load_all_vocabs_from_github():
                 WHERE {
                     ?uri a owl:Ontology .
                     ?uri2 a skos:ConceptScheme ;
-                        skos:prefLabel ?pl .
+                        skos:prefLabel ?name .
                 }
             '''
             for r in g.query(q):
-                base_url = r['uri']
                 vocab_id = r['uri'].split('/')[-1]
-                label = r['pl']
+                base_url = r['uri']
+                name = r['pl']
 
-                load_one_vocab_from_github(vocab_id, base_url, label)
+                print('{} {} {}'.format(vocab_id, base_url, name))
+                #load_one_vocab_from_github(repo_id, vocab_id, base_url, label)
 
 
 def list_repositories():
@@ -145,39 +154,52 @@ def list_repositories():
     return json.loads(r.content.decode('utf-8'))
 
 
-def make_config_file_contents(template_file, base_url, vocab_id, label):
-    with open(template_file, 'r') as f:
-        config_template_contents = f.read()
-        return config_template_contents\
-                    .replace('{repository_id}', vocab_id)\
-                    .replace('{label}', label)\
-                    .replace('{base_url}', base_url)
-
-
-def create_repository(config_template_file_path, base_url, repo_id, repo_label):
-    config_contents = make_config_file_contents(config_template_file_path,  base_url, repo_id, repo_label)
-
-    r = requests.post(
-        config.GRAPHDB_REPO_URI,
-        files={'config': ('config.ttl', config_contents)},
+def delete_repo(repo_id):
+    r = requests.delete(
+        config.GRAPHDB_REPOS_URI + '/' + repo_id,
         auth=(config.GRAPHDB_USR, config.GRAPHDB_PWD)
     )
 
-    return r.status_code
+    if r.status_code == 200:
+        print('Deleted repo {}'.format(repo_id))
+    else:
+        print('ERROR: did not delete repo {}, message: {}'.format(repo_id, r.text))
 
 
-def delete_repo(vocab_id):
-    uri = config.GRAPHDB_REPO_URI + '/' + vocab_id
-    r = requests.delete(
-        uri,
-        auth=(config.GRAPHDB_USR, config.GRAPHDB_PWD),
-        headers={'Content-Type': 'application/json', 'Accept': 'application/json'}
+def make_repo_config_file(template_file, base_url, repo_id, repo_label):
+    with open(template_file, 'r') as f:
+        config_template_contents = f.read()
+        return config_template_contents\
+                    .replace('{repo_id}', repo_id)\
+                    .replace('{repo_label}', repo_label)\
+                    .replace('{base_url}', base_url)
+
+
+def create_repo(repo_config):
+    r = requests.post(
+        config.GRAPHDB_REPOS_URI,
+        files={'config': ('config.ttl', repo_config)},
+        auth=(config.GRAPHDB_USR, config.GRAPHDB_PWD)
     )
 
-    if r.status_code != 204:
-        print(r.content.decode('utf-8'))
-    return r.status_code
+    if r.status_code == 201:
+        print('Created repo {}'.format(repo_id))
+    else:
+        print('ERROR: did not create repo {}, message: {}'.format(repo_id, r.text))
 
 
 if __name__ == '__main__':
-    load_all_vocabs_from_github()
+    repo_id = 'GSQ_Vocabularies_core'
+    repo_name = 'GSQ Vocabularies'
+
+    # delete_repo(repo_id)
+    #
+    # repo_config = make_repo_config_file(
+    #     'repo-config.ttl.template',
+    #     'http://linked.data.gov.au/def/',
+    #     repo_id,
+    #     repo_name
+    # )
+
+    # # load_all_vocabs_from_github()
+    load_all_background_onts_from_github(repo_id)
