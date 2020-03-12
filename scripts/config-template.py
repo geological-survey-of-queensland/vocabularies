d74d41651e6e321038da86c2fa593bae73232e25
David Crosswell <47953763+DavidCrosswellGSQ@users.noreply.github.com>
2020-03-13 09:40:36 +1000

    Update config-template.py

--- scripts/config-template.py
+++ scripts/config-template.py
@@ -1,5 +1,10 @@
-GRAPHDB_REPO_URI = 'http://.../rest/repositories'
-GRAPHDB_LOAD_DATA_URI = 'http://.../rest/data/import/upload/.../url'
-GITHUB_RAW_URI_BASE = 'https://...'
-GRAPHDB_USR = '...'
-GRAPHDB_PWD = '...'
+GRAPHDB_REPO_ID = 'GSQ_Vocabularies_Master'  # set repo as needed
+GRAPHDB_REPO_URI = 'https://graphdb.test.gsq.digital/repositories/{}'.format(GRAPHDB_REPO_ID)  # set repo URI as needed
+GRAPHDB_LOAD_DATA_URI = 'https://graphdb.test.gsq.digital/rest/data/import/upload/{}/url'.format(GRAPHDB_REPO_ID)  # set API URI as needed
+GRAPHDB_USR = 'graphdb_username'
+GRAPHDB_PWD = 'graphdb_password'
+
+GITHUB_RAW_URI_BASE = 'https://raw.githubusercontent.com/geological-survey-of-queensland/vocabularies/master/vocabularies/'
+
+GITHUB_USR = 'github_username'  # or any valid GitHub account
+GITHUB_PWD = 'github_password'  # for the valid GitHub account
