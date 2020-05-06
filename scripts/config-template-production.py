06efbac435681ccfd9a04f644e1820b4a20e46eb
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-05-07 09:07:49 +1000

    addressing Vance's comment

--- scripts/config-template-production.py
+++ scripts/config-template-production.py
@@ -1,10 +1,10 @@
 # Configuration file for vocabs_load.py file. Edit this template and save as config.py
 
-GRAPHDB_REPO_ID = 'GSQ_Vocabularies_Master' 
-GRAPHDB_REPO_URI = 'https://graphdb.gsq.digital/repositories/{}'.format(GRAPHDB_REPO_ID) 
-GRAPHDB_LOAD_DATA_URI = 'https://graphdb.gsq.digital/rest/data/import/upload/{}/url'.format(GRAPHDB_REPO_ID) 
-GRAPHDB_USR = 'graphdb_username'
-GRAPHDB_PWD = 'graphdb_password'
+GRAPHDB_REPO_ID = 'vocabs'
+GRAPHDB_REPO_URI = 'http://localhost:7200/repositories/{}'.format(GRAPHDB_REPO_ID)
+GRAPHDB_LOAD_DATA_URI = 'http://localhost:7200/rest/data/import/upload/{}/url'.format(GRAPHDB_REPO_ID)
+GRAPHDB_USR = ''
+GRAPHDB_PWD = ''
 
 GITHUB_RAW_URI_BASE = 'https://raw.githubusercontent.com/geological-survey-of-queensland/vocabularies/master/vocabularies/'
 
